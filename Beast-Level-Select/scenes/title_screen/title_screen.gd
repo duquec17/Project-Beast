44b83a25c37375dedb43af254c4f896c3a4e2126
Cristian <duquec@sunypoly.edu>
2024-10-23 14:09:23 -0400

    New title/menu UI start
    Starting to develop the beginning UI for game start which will shift to different areas based on button pressed.

---
+++ Beast-Level-Select/scenes/title_screen/title_screen.gd
@@ -0,0 +1,22 @@
+extends Control
+
+## One Any button pressed move to other buttons
+func _on_any_button_pressed():
+	pass # Replace with function body.
+
+## Searches for a empty game save to start writing new content into
+func _on_new_game_pressed():
+	pass # Replace with function body.
+
+## Opens menu for all game saves
+func _on_load_game_pressed():
+	pass # Replace with function body.
+
+## Opens configuration menu 
+func _on_config_pressed():
+	pass # Replace with function body.
+
+## On press exit button
+func _on_exit_pressed():
+	get_tree().quit()
+
